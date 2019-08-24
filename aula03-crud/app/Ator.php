f03fe79c12620753ef4856f0daf8cad3d089a640
almeidarenato <renatoam@ymail.com>
2019-08-23 21:31:28 -0300

    Criado Model Ator e Genero

--- aula03-crud/app/Ator.php
+++ aula03-crud/app/Ator.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ator extends Model
 {
-    //
+    protected $table = "atores"; //referencia a tabela que já existe
+    protected $primaryKey = "id"; // chave primária
+    protected $fillable = ['nome'];
 }
--- aula03-crud/app/Genero.php
+++ aula03-crud/app/Genero.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genero extends Model
 {
-    //
+    protected $table = "generos"; //referencia a tabela que já existe
+    protected $primaryKey = "id"; // chave primária
+    protected $fillable = ['descricao'];
 }
